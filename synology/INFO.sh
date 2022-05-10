69b47a4d8b81e404807e8159bf8321f13547a27e
xrad <dargllun@gmail.com>
2022-05-10 20:25:07 +0200

    Fix INFO for DSM7, remove dsmui key

--- synology/INFO.sh
+++ synology/INFO.sh
@@ -6,11 +6,10 @@ source /pkgscripts/include/pkg_util.sh
 package="LogitechMediaServer"
 version="8.2.1-1649774087"
 displayname="Logitech Media Server"
-os_min_ver="6.0-00000"
+os_min_ver="7.0-40000"
 maintainer="xrad"
 arch="$(pkg_get_platform)"
 startable="yes"
 description="Logitech Media Server Package"
-dsmuidir="ui"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
